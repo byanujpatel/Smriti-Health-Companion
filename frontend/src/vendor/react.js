a03efacc40a60f30800d8ad5aa0025a41702a161
byanujpatel <anujpatel2899@gmail.com>
2026-07-16 09:06:12 +0530

    Vendor React for local UI rendering

---
+++ frontend/src/vendor/react.js
@@ -0,0 +1,7 @@
+const React = window.React;
+
+export const useEffect = React.useEffect;
+export const useMemo = React.useMemo;
+export const useRef = React.useRef;
+export const useState = React.useState;
+export default React;
